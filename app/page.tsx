f2a4dac8646f795786953aab00fc664afc80102b
MarkPagkaliwagan <info.markpagkaliwagan@gmail.com>
2026-02-26 14:54:54 +0800

    jtge

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,5 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { supabase } from "../lib/supabaseClient";
-
-interface Student {
-  id: number;
-  name: string;
-  age: number;
-  created_at: string;
-}
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const [students, setStudents] = useState<Student[]>([]);
-
-  useEffect(() => {
-    const fetchStudents = async () => {
-      const { data, error } = await supabase
-        .from("students")
-        .select("*");
-
-      if (error) {
-        console.log(error.message);
-      } else {
-        setStudents(data as Student[]); // 👈 cast na lang
-      }
-    };
-
-    fetchStudents();
-  }, []);
-
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Students</h1>
-
-      {students.map((student) => (
-        <div key={student.id}>
-          <p>{student.name}</p>
-          <p>{student.age}</p>
-          <hr />
-        </div>
-      ))}
-    </div>
-  );
+  redirect("/module-2-inspection/login");
 }
