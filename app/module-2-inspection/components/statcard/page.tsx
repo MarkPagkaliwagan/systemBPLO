58dff2f2002e43545c6f98dfec99e49d9d20cd4b
Erica <ecashane18@gmail.com>
2026-02-27 12:50:00 +0800

    stat cards

--- app/module-2-inspection/components/statcard/page.tsx
+++ app/module-2-inspection/components/statcard/page.tsx
@@ -6,13 +6,15 @@ interface StatCardProps {
 
 export default function StatCard({ title, value }: StatCardProps) {
   return (
-    <div className="bg-[#B3B3B3] rounded-[45px] p-6 flex flex-col items-center justify-center shadow-sm w-full h-50">
-      <p className="text-gray-800 text-lg font-medium mb-2 capitalize">
-        {title}
-      </p>
-      <h2 className="text-6xl font-bold text-black">
-        {value}
-      </h2>
+    <div className="ml-80">
+      <div className="bg-[#B3B3B3] rounded-[45px] p-6 flex flex-col items-center justify-center shadow-sm w-full h-50">
+        <p className="text-gray-800 text-lg font-medium mb-2 capitalize">
+          {title}
+        </p>
+        <h2 className="text-6xl font-bold text-black">
+          {value}
+        </h2>
+      </div>
     </div>
   );
 }
