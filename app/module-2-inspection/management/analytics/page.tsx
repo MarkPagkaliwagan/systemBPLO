bdaa588e2b5f523599034e1043d8f5cfd36fd395
lendrick <lendrickdev06@gmail.com>
2026-02-27 10:49:42 +0800

    fix import

--- app/module-2-inspection/management/analytics/page.tsx
+++ app/module-2-inspection/management/analytics/page.tsx
@@ -1,8 +1,10 @@
 import StatCard from "../../components/statcard/page"; 
+import Sidebar from "../../components/sidebar/page";
 
 export default function AnalyticsDashboard() {
   return (
     <div className="flex h-screen bg-[#D9D9D9]">
+      <Sidebar />
       <main className="flex-1 flex flex-col">
         <header className="bg-[#B7E4A1] h-16 flex items-center justify-center border-b border-gray-400">
           <h1 className="text-xl font-medium text-black">
