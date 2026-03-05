03662e0d88ede6a2d2edb60e1ee7430c5b21b4b3
MarkPagkaliwagan <info.markpagkaliwagan@gmail.com>
2026-03-05 15:40:46 +0800

    fix reset password suspense error

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   redirect("Admin/Inspection/management/analytics");
   redirect("Admin/Inspection/masterlist");
   redirect("Admin/Inspection/management/review");
-  redirect("Admin/Compliance/Dashboard");
+  redirect("Admin/Compliance/");
   redirect("Admin/Compliance/Aging")
   
 }
