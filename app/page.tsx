a18c8a3e605cbbf44dab7cccb6d2a339ac62b0a1
Erica <ecashane18@gmail.com>
2026-03-05 14:23:13 +0800

    push

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("Admin/module-2-inspection/login");
-  redirect("Admin/module-2-inspection/components/sidebar");
-  redirect("Admin/module-2-inspection/management/analytics");
-  redirect("Admin/module-2-inspection/management/analytics/masterlist");
-  redirect("Admin/module-2-inspection/management/analytics/review");
-  redirect("Admin/module-3-notice/Dashboard");
-  redirect("Admin/module-3-notice/Aging")
+  redirect("/login");
+  redirect("/components/sidebar");
+  redirect("Admin/Inspection/management/analytics");
+  redirect("Admin/Inspection/masterlist");
+  redirect("Admin/Inspection/management/review");
+  redirect("Admin/Compliance/Dashboard");
+  redirect("Admin/Compliance/Aging")
   
 }
