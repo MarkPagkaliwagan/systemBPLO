6367281731202f353deaed8446dbb685e110979c
MarkPagkaliwagan <info.markpagkaliwagan@gmail.com>
2026-03-09 16:37:34 +0800

    fix prisma generate for vercel

--- app/SuperAdmin/Compliance/Dashboard/page.tsx
+++ app/SuperAdmin/Compliance/Dashboard/page.tsx
@@ -440,7 +440,7 @@ export default function DashboardPage() {
             </div>
           </div>
 
-          {/* Modal */}
+          {/* Modbgfgal */}
           {selectedViolation && (
             <div className="fixed inset-0 z-[100] flex items-center justify-center bg-black/60 backdrop-blur-sm p-4">
               <DetailsFerBusesForm
